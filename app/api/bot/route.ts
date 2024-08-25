c6cbcf2246b7b48c4ed97f6ff4389acc79533ed5
David Hurley <dhurls99@gmail.com>
2024-08-25 17:16:08 -0400

    print bot

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -10,7 +10,9 @@ if (!token)
   throw new Error("TELEGRAM_BOT_TOKEN environment variable not found.");
 
 const bot = new Bot(token);
+
 bot.on("message:text", async (ctx) => {
+  console.log(ctx.message.text);
   await ctx.reply(ctx.message.text);
 });
 
