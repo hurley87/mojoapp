8550996951be8760c0a1da5221767cdb1674adf2
David Hurley <dhurls99@gmail.com>
2024-08-29 11:33:10 -0400

    do it live

--- components/get-started.tsx
+++ components/get-started.tsx
@@ -3,13 +3,24 @@
 import { usePrivy } from '@privy-io/react-auth';
 
 export default function GetStarted() {
-  const { user, login } = usePrivy();
+  const { user, login, logout } = usePrivy();
   const username = user?.telegram?.username;
 
   return (
     <div>
-      {user && <div>{username}</div>}
-      {!user && <button onClick={login}>Login</button>}
+      {user && (
+        <div className="flex flex-col gap-3">
+          <div>{username}</div>
+          <button onClick={logout} className="bg-red-500 text-white">
+            Logout
+          </button>
+        </div>
+      )}
+      {!user && (
+        <div className="flex flex-col gap-3">
+          <button onClick={login}>Login</button>
+        </div>
+      )}
     </div>
   );
 }
