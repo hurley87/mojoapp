ae1f43df9376ec91c3db2fe02e91e6704f5a2c7e
David Hurley <dhurls99@gmail.com>
2024-08-29 11:27:55 -0400

    add username to app

--- components/get-started.tsx
+++ components/get-started.tsx
@@ -5,9 +5,10 @@ import { usePrivy } from '@privy-io/react-auth';
 export default function GetStarted() {
   const { user, login } = usePrivy();
   console.log(user);
+  const username = user?.telegram?.username;
   return (
     <div>
-      {user && <div>you are in</div>}
+      {user && <div>{username}</div>}
       {!user && <button onClick={login}>Login</button>}
     </div>
   );
