051387a6fc5558016d3a95efef63209d3abaac53
David Hurley <dhurls99@gmail.com>
2024-08-29 10:26:27 -0400

    refactor Telegram bot

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -12,32 +12,20 @@ if (!token)
 const bot = new Bot(token);
 
 bot.on('message:text', async (ctx) => {
-  console.log(ctx);
   const message = ctx.message;
-  console.log(message);
 
   if (message.text === '/start') {
-    const chatId = message.chat.id;
+    const chat_id = message.chat.id;
     const url = 'https://www.mojo.club';
 
     const response = {
-      chat_id: chatId,
-      text: 'Welcome! Click the button below to open the mini-app.',
+      chat_id,
+      text: 'Welcome! Click the button below to open the Mono Mini-App.',
       reply_markup: {
-        // keyboard: [
-        //   [
-        //     {
-        //       text: 'Open Mini App',
-        //       web_app: {
-        //         url: webAppUrl,
-        //       },
-        //     },
-        //   ],
-        // ],
         inline_keyboard: [
           [
             {
-              text: 'Mojo App',
+              text: 'Mono Mini-App',
               web_app: { url },
             },
           ],
@@ -45,14 +33,10 @@ bot.on('message:text', async (ctx) => {
       },
     };
 
-    await ctx.api.sendMessage(chatId, response.text, {
+    await ctx.api.sendMessage(chat_id, response.text, {
       reply_markup: response.reply_markup,
     });
-  } else {
-    await ctx.reply('I do not understand you.');
   }
-
-  // await ctx.reply(ctx.message.text);
 });
 
 export const POST = webhookCallback(bot, 'std/http');
